b4e3ea3526f03dfd03cded88c40e5a79ac6a181a
Eric Xiong <exiong@ualberta.ca>
2026-05-01 18:35:48 -0600

    add product/tag/category model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField()
+
+    # We protect products by forcing a category to be empty of products before removing one.
+    # Another good option is to set a new default category when a category containing prods are deleted.
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        related_name="products"
+    )
+
+
+    tags = models.ManyToManyField(
+        Tag,
+        related_name="products",
+        blank=True
+    )
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
